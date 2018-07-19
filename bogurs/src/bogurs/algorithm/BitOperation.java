716890c09a63149f98a9e94ccdc233a6381afaf4
thsong <thsong@192.168.0.7>
2018-07-19 16:25:42 +0900

    비트마스크 백준님 문제 예제 (11723)

---
+++ bogurs/src/bogurs/algorithm/BitOperation.java
@@ -0,0 +1,45 @@
+package bogurs.algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BitOperation {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(bf.readLine());
+		int maxN = 20;
+		
+		int res=0;
+		String line = null;
+		String[] lintSpl = null;
+		int x;
+		StringBuilder sb = new StringBuilder();
+		while(n-- > 0){
+			line = bf.readLine();
+			if("all".equals(line)){
+				res = (1<<(maxN+1))-1;
+			} else if("empty".equals(line)){
+				res = 0;
+			} else {
+				lintSpl = line.split(" ");
+				x = Integer.parseInt(lintSpl[1]);
+				if("remove".equals(lintSpl[0])){
+					res = res&~(1<<x);
+				} else if("check".equals(lintSpl[0])){
+					if((res&(1<<x))!=0){
+						sb.append("1\n");
+					} else {
+						sb.append("0\n");
+					}
+				} else if("toggle".equals(lintSpl[0])){
+					res = res^(1<<x);
+				} else if("add".equals(lintSpl[0])){
+					res = res|(1<<x);
+				} 
+			}
+		}
+		System.out.println(sb);
+	}
+}
